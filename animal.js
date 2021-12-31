aaccccd1559624efa27ece82b2c3d32a36bce459
bachngocng <nnbach97@gmail.com>
2021-12-31 11:06:52 +0700

    done

---
+++ animal.js
@@ -0,0 +1,30 @@
+class Animal {
+    _name;
+    _weight;
+
+    constructor(name, weight) {
+        this._name = name;
+        this._weight = weight;
+    }
+
+    get name() {
+        return this._name;
+    }
+
+    set name(value) {
+        this._name = value;
+    }
+
+    get weight() {
+        return this._weight;
+    }
+
+    set weight(value) {
+        this._weight = value;
+    }
+
+    toString() {
+        return this._name + " " + this._weight
+    }
+
+}
